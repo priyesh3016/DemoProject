78efd17f8fe1ba44f2388c672994581323785f8f
yash <patil998707@gmail.com>
2026-02-06 18:24:50 +0530

    api practice

--- src/ComplexJsonParse.java
+++ src/ComplexJsonParse.java
@@ -5,6 +5,37 @@ public class ComplexJsonParse {
 	
 	public static void main(String[]args) {
 		JsonPath  js = new  JsonPath(payload.coursePrize());
+		//print number  of courses  returned by API
+	
+		int count = js.getInt("courses.size()");
+		System.out.println(count);
+		
+		
+		//print purchase  amount
+		
+		int totalAmount = js.get("dashboard.purchaseAmount");
+		System.out.println(totalAmount);
+		
+		
+		//print title  of  the  first course 
+		
+		String firstCourseTitle = js.get("courses[0].title");
+		System.out.println(firstCourseTitle);
+		
+		
+		//print all courses titles and  their respective  price 
+		
+		for(int i=0;i<count;i++) 
+		{
+			String courseWithTitle = js.get("courses["+i+"].title");
+		    System.out.println(js.get("courses["+i+"].price").toString());
+			System.out.println(courseWithTitle);
+			
+		}
+		
+		
+		
+		
 	}
 
 }
